74b1654c9f6c0d309f86847e345e21f8b71b59e7
Roger Sobrado <rsobrado@goduesouth.com>
2017-05-29 21:00:17 -0400

    loading list data from json

---
+++ src/data/workers.js
@@ -0,0 +1,29 @@
+[
+  {
+    "id" : "0011222",
+    "firstName" : "Tomas",
+    "lastName" : "Flores",
+    "phoneNumber" : "4078899490",
+    "email" : "tflores@gmail.com",
+    "role" : "Painter",
+    "hourRate" : "10"
+  },
+  {
+    "id" : "0011233",
+    "firstName" : "Royer",
+    "lastName" : "Rubin",
+    "phoneNumber" : "5617979490",
+    "email" : "rrubin@gmail.com",
+    "role" : "Manager",
+    "hourRate" : "17"
+  },
+  {
+    "id" : "0011222",
+    "firstName" : "Diego",
+    "lastName" : "Elizondo",
+    "phoneNumber" : "3218899490",
+    "email" : "delizondo@gmail.com",
+    "role" : "CEO",
+    "hourRate" : "20"
+  }
+]
